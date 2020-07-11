476b0cb731b5bcfcb64f4f75232004e0333f0f2c
Saleem Khan <saleemkce@gmail.com>
2020-07-11 21:47:52 +0530

    Added cache control to support IOS device ajax call

--- backend/php/database.php
+++ backend/php/database.php
@@ -2,7 +2,7 @@
 session_start();
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Accept, Authorization, X-Requested-With, App-Session, App-Userid');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Accept, Authorization, X-Requested-With, App-Session, App-Userid, Cache-Control');
 
 class Database extends Exception {
     protected $serverName;
